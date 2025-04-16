2f9a297db06810ee04e939e1e8fb879375cc09a8
harish45264 <harishvishwa003@gmail.com>
2025-04-16 06:21:25 +0530

    Added Project Structure

---
+++ src/main/java/com/codeEval/CodeEval/CodeEvalApplication.java
@@ -0,0 +1,13 @@
+package com.codeEval.CodeEval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeEvalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeEval/CodeEval/CodeEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeEval.CodeEval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
