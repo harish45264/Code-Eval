9d32069acaf0718bee2cb4e9ba8431fbcbb0e540
Bagath <132096909+Bagathexe@users.noreply.github.com>
2025-04-18 23:56:50 +0530

    Files updated

---
+++ src/main/java/com/EE/CodeEval/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.EE.CodeEval.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.EE.CodeEval.repository.*;
+import com.EE.CodeEval.model.*;
+
+@SuppressWarnings("unused")
+// @RestController
+
+@RequiredArgsConstructor
+public class TestController {
+    private final ProblemRepository repo;
+
+
+    
+    @GetMapping("/test")
+    public String test() {
+        repo.save(new Problem(null, "Two Sum", "Easy", "Find two numbers..."));
+        return "Inserted!";
+    }
+}
+
