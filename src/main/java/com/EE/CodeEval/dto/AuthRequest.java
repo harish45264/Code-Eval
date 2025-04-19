28a1113473714ab473f683efd4702f877b5bcb15
Bagath <132096909+Bagathexe@users.noreply.github.com>
2025-04-19 16:31:33 +0530

    k

--- src/main/java/com/EE/CodeEval/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.EE.CodeEval.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String username;
-    private String password;
-}
--- src/main/java/com/EE/CodeEval/repository/ProblemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.EE.CodeEval.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.EE.CodeEval.model.Problem;
-
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
-}
