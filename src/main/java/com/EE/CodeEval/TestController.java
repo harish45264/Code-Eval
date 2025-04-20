4c7bcb0a977d2b824d327406396b5e85390ad298
Bagath <132096909+Bagathexe@users.noreply.github.com>
2025-04-20 17:08:49 +0530

    Small errors fixed

--- src/main/java/com/EE/CodeEval/TestController.java
+++ src/main/java/com/EE/CodeEval/TestController.java
@@ -3,8 +3,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import com.EE.CodeEval.repository.ProblemRepository;
-import com.EE.CodeEval.model.*;
+// import com.EE.CodeEval.repository.ProblemRepository;
+// import com.EE.CodeEval.model.*;
 
 @RestController
 @RequiredArgsConstructor
